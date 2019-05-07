a3823fc83082c4584a623cebe581cbe1eecf014d
Rexben <rexben.rb@gmail.com>
2019-05-07 12:51:41 +0100

    fixed array methods task

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,43 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(names => fullName.push(`${names.first_name} ${names.lastname}`));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+allCaps = runners.map(names => names.first_name.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter(names => names.shirt_size == 'L');
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+let price = runners.map(names => names.donation);
+ticketPriceTotal = price.reduce(function(accumulator, currentValue, currentIndex, array) {
+    return accumulator + currentValue;
+  });
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+let attendance = [
+    { id: 1, name: 'Ben', age: 18 },
+    {id: 2, name: 'Benson', age: 28},
+    {id: 3, name: 'Tom', age: 10},
+    {id: 4, name: 'Laide', age: 16},
 
+]
 // Problem 1
 
 // Problem 2
-
+//Screen out those that are not up to 18 years
+let above18years = attendance.filter(attendees => attendees.age >= 18);
+console.log(above18years);
 // Problem 3
